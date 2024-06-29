acee4d44328ed5941314273bf5b8f4d45896c4d9
pencilgon_ <82328945+Ulman-huy@users.noreply.github.com>
2024-06-29 23:25:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ class Main {
             if (currentTime >= this.newYearDisplayTime) {
                 this.context.fillStyle = WHITE;
                 this.context.font = `${FONT_SIZE}px Arial`;
-                this.context.fillText("Anh yêu em nhìu <3", WINDOWWIDTH / 2, WINDOWHEIGHT / 2);
+                this.context.fillText("HAPPY NEW YEAR!", WINDOWWIDTH / 2, WINDOWHEIGHT / 2);
             }
         }
 
